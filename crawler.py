4b65b4ff79a0250e506e62a16a9ec8054936fc1f
rudalsss <linda284@naver.com>
2025-10-17 20:55:33 +0900

    feat: 한국개발연구원(kdi) 시사용어사전 크롤링 구현

--- crawler.py
+++ crawler.py
@@ -1,4 +1,5 @@
 import requests, re, html
+from bs4 import BeautifulSoup
 from tqdm import tqdm
 
 def clean_txt(raw:str) -> str:
@@ -29,12 +30,50 @@ def crawl_bok():
         results.append((keyword, desc_short))
         tqdm.write(f"{i}번 {keyword}")  # tqdm-safe 출력
 
-    print("\n✅ 크롤링 완료!")
+    print("\n✅ BOK 크롤링 완료!")
     print(f"총 {len(results)}건 수집됨\n")
 
     # 결과셋 터미널 출력(1000자)
     for r in results[:3]:
-        print(f"• {r[0]} → {r[1][:1000]}...")
+        print(f"• {r[0]} → {r[1][:1000]}")
+
+def crawl_kdi():
+    base_url = "https://eiec.kdi.re.kr/material/wordDicDetail.do?dic_idx="
+    results = []
+
+    for i in tqdm(range(1,10)):
+        #url 세팅후 요청
+        url = f"{base_url}{i}"
+        res = requests.get(url, headers={"User-Agent": "Mozilla/5.0"}, verify=False)
+        if res.status_code != 200:
+            continue
+
+        soup = BeautifulSoup(res.text, "html.parser")
+
+        # 용어명 : <dt>
+        term = soup.select_one("dt")
+        term_txt = term.get_text(strip=True) if term else f"no_title_{i}"
+
+        # 용어명 : <dd>
+        desc = soup.select_one("dd")
+        if desc:
+            desc_txt = desc.get_text(strip=True)
+            desc_short = ". ".join(desc_txt.split(". ")[:3]) + "."
+        else:
+            desc_short = ""
+
+
+        # 결과 저장
+        results.append((term_txt, desc_short))
+        tqdm.write(f"{i}번 {term_txt}")
+    
+    print("\n✅ KDI 크롤링 완료!")
+    print(f"총 {len(results)}건 수집됨\n")
+
+    # 결과셋 터미널 출력(1000자)
+    for r in results[:3]:
+        print(f"• {r[0]} → {r[1][:4000]}")
 
 if __name__ == "__main__":
     crawl_bok()
+    crawl_kdi()
