6b363d3c58327be685d4359b76e54ee306bedde9
rudalsss <linda284@naver.com>
2025-10-17 14:34:34 +0900

    feat: 순서텍스트 정제작업(clean_txt)

--- crawler.py
+++ crawler.py
@@ -1,5 +1,11 @@
-import requests
+import requests, re, html
 from tqdm import tqdm
+
+def clean_txt(raw:str) -> str:
+    text = html.unescape(raw)   # HTML 엔티티 복원
+    text = re.sub(r"<[^>]+>", "", text)     # HTML 태그제거
+    text = re.sub(r"\s+", " ", text)        # 공백정리
+    return text.strip()
 
 def crawl_bok():
     base_url = "https://www.bok.or.kr/portal/ecEdu/ecWordDicary/searchCont.json?ecWordSn="
@@ -7,22 +13,21 @@ def crawl_bok():
 
     for i in tqdm(range(1, 10)):
         # url세팅후 요청
-        url = f"{base_url}{i}"
-        res = requests.get(url)
-
+        res = requests.get(f"{base_url}{i}")
         if res.status_code != 200:
             continue
+
         data = res.json().get("result")
         if not data:
             continue
 
         # 응답 데이터 파싱후 결과셋에 추가
         keyword = data.get("ecWordNm")
-        desc = data.get("ecWordCn", "").replace("\n", " ").strip()
+        desc = clean_txt(data.get("ecWordCn", ""))
         desc_short = ". ".join(desc.split(".")[:2]) + "."
 
         results.append((keyword, desc_short))
-        tqdm.write(f"{i}번 {keyword}")  # tqdm-safe 출력!
+        tqdm.write(f"{i}번 {keyword}")  # tqdm-safe 출력
 
     print("\n✅ 크롤링 완료!")
     print(f"총 {len(results)}건 수집됨\n")
